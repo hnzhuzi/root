c0f984c898e5f7bb5c382fb65b5ac658801861e0
hnzhuzi <49380375+hnzhuzi@users.noreply.github.com>
2020-07-25 14:27:05 +0800

    Update init.sh

--- other/init.sh
+++ other/init.sh
@@ -8,7 +8,7 @@ rm -rf /etc/yum.repos.d/*
 wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
 wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
 yum clean all
-yum -y install bash-completion bash-completion-extras net-tools vim-enhanced tree wget git iptables-services
+yum -y install bash-completion bash-completion-extras net-tools vim-enhanced tree wget git iptables-services ntpdate
 
 chattr -i /etc/ssh/sshd_config
 sed -i 's#PermitRootLogin no#PermitRootLogin yes#' /etc/ssh/sshd_config
