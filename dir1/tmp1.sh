5f0a9a7334b87f407464c92bef48dd168553cd7f
root <root@gmail.com>
2020-10-13 14:58:11 +0800

    20201013

---
+++ dir1/tmp1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+for v1 in $(ls /root/cluster/); do
+  kubectl --kubeconfig cluster/$v1 get pod >/dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    if kubectl --kubeconfig cluster/$v1 get pod --all-namespaces | grep -q $1; then
+      echo $v1 ok!
+    else
+      continue
+    fi
+  else
+    echo "$v1 can't access!"
+  fi
+done
+
